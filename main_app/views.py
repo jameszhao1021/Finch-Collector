b9bbf2112fc762a445b8353869b742ae42de280a
james <351917842@qq.com>
2024-04-11 03:08:48 +0800

    complete version

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from .models import Finch
+from .models import Finch, Toy
 from .forms import FeedingForm
 # Create your views here.
 
@@ -21,11 +21,23 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = get_object_or_404(Finch, pk=finch_id)
+    current_toy_ids = finch.toys.all().values_list('id')
+    available_toys = Toy.objects.exclude(id__in=current_toy_ids)
     feeding_form = FeedingForm()
     return render (request, 'finches/detail.html', {
         'finch': finch,
-         "feeding_form": feeding_form
+         "feeding_form": feeding_form,
+         'available_toys': available_toys
     })
+
+def add_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.add(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
+def remove_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.remove(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
 
 class FinchCreate(CreateView):
     model = Finch
@@ -33,19 +45,33 @@ class FinchCreate(CreateView):
 
 def add_feeding(request, finch_id):
   # create a ModelForm instance using the data in request.POST
-  form = FeedingForm(request.POST)
+    form = FeedingForm(request.POST)
   # validate the form
-  if form.is_valid():
- 
-    new_feeding = form.save(commit=False)
-    new_feeding.finch_id = finch_id
-    new_feeding.save()
-  return redirect('detail', finch_id=finch_id)
-
-class FinchUpdate(UpdateView):
-    model = Finch
     fields = ['breed', 'description', 'age']
 
 class FinchDelete(DeleteView):
     model = Finch
     success_url = '/finches'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['breed', 'description', 'age']
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
